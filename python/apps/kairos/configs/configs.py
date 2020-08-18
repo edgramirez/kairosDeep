44d67fe5a3f306a3ad74d3946eaeceaa9355f4da
edgramirez <edgar.ramirez@mit-mexico.com>
2020-08-18 23:36:32 +0000

    typo

--- python/apps/kairos/configs/configs.py
+++ python/apps/kairos/configs/configs.py
@@ -12,10 +12,8 @@ config = {
             },
 
         'services': {
-              # 1 defines A1 as outside, 2 defines A2 as outside area 
+            # 1 defines A1 as outside, 2 defines A2 as outside area 
             'aforo': {'enabled': True,'outside_area': 1, 'aforo_reference_line_coordinates': [(510, 740), (1050, 740)]},
             'people_counting': {'enabled': False,'frequency': 32},
             'social_distance': {'enabled': True,'tolerated_distance': 150,'persistence_time': 1,'enabled_draw_line': False,'enabled_draw_rectangle': False,'line_width': 3,'line_color': (50, 120 ,255)}},
-            '},
-
         }
