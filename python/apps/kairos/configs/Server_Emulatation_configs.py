c10ff4edd3ca9a5d53d9cd539f43a6c0e670ca3d
edgramirez <edgar.ramirez@mit-mexico.com>
2020-08-26 18:25:55 +0000

    nueva

--- python/apps/kairos/configs/Server_Emulatation_configs.py
+++ python/apps/kairos/configs/Server_Emulatation_configs.py
@@ -13,23 +13,47 @@ config = {
 
         'cameras': {
             'Negocio0001-Tienda12-fe80::adc4:3105:77aa:111': {
-                'aforo': {'enabled': True,'outside_area': 1, 'aforo_reference_line_coordinates': [(510, 740), (1050, 740)]},
-                'social_distance': {'enabled': False,'tolerated_distance': 150,'persistence_time': 1,'enabled_draw_line': False,'enabled_draw_rectangle': False,'line_width': 3,'line_color': (50, 120 ,255)},
+                'aforo': {
+                    'enabled': True,
+                    'outside_area': 1, 
+                    'aforo_reference_line_coordinates': [(510, 700), (1100, 700)], 
+                    'line_width': 5,
+                    'line_color': [1, 1, 1, 1],
+                    },
+                'social_distance': {'enabled': False,'tolerated_distance': 150,'persistence_time': 1,'line_width': 5,'line_color': (50, 120 ,255)},
                 'people_counting': {'enabled': False},
                 },
             'Negocio0001-Tienda12-fe80::adc4:3105:77aa:222': {
-                'aforo': {'enabled': True,'outside_area': 1, 'aforo_reference_line_coordinates': [(510, 740), (1050, 740)]},
-                'social_distance': {'enabled': False,'tolerated_distance': 150,'persistence_time': 1,'enabled_draw_line': False,'enabled_draw_rectangle': False,'line_width': 3,'line_color': (50, 120 ,255)},
+                'aforo': {
+                    'enabled': True,
+                    'outside_area': 1, 
+                    'aforo_reference_line_coordinates': [(510, 700), (1100, 700)], 
+                    'line_width': 5,
+                    'line_color': [1, 1, 1, 1],
+                    },
+                'social_distance': {'enabled': False,'tolerated_distance': 150,'persistence_time': 1,'line_width': 5,'line_color': (50, 120 ,255)},
                 'people_counting': {'enabled': False},
                 },
             'Negocio0001-Tienda12-fe80::adc4:3105:77aa:333': {
-                'aforo': {'enabled': True,'outside_area': 1, 'aforo_reference_line_coordinates': [(510, 740), (1050, 740)]},
-                'social_distance': {'enabled': False,'tolerated_distance': 150,'persistence_time': 1,'enabled_draw_line': False,'enabled_draw_rectangle': False,'line_width': 3,'line_color': (50, 120 ,255)},
+                'aforo': {
+                    'enabled': True,
+                    'outside_area': 1, 
+                    'aforo_reference_line_coordinates': [(510, 700), (1100, 700)], 
+                    'line_width': 5,
+                    'line_color': [1, 1, 1, 1],
+                    },
+                'social_distance': {'enabled': False,'tolerated_distance': 150,'persistence_time': 1,'line_width': 5,'line_color': (50, 120 ,255)},
                 'people_counting': {'enabled': False},
                 },
             'Negocio0001-Tienda12-fe80::adc4:3105:77aa:444': {
-                'aforo': {'enabled': True,'outside_area': 1, 'aforo_reference_line_coordinates': [(510, 740), (1050, 740)]},
-                'social_distance': {'enabled': False,'tolerated_distance': 150,'persistence_time': 1,'enabled_draw_line': False,'enabled_draw_rectangle': False,'line_width': 3,'line_color': (50, 120 ,255)},
+                'aforo': {
+                    'enabled': True,
+                    'outside_area': 1, 
+                    'aforo_reference_line_coordinates': [(510, 700), (1100, 700)], 
+                    'line_width': 5,
+                    'line_color': [1, 1, 1, 1],
+                    },
+                'social_distance': {'enabled': False,'tolerated_distance': 150,'persistence_time': 1,'line_width': 5,'line_color': (50, 120 ,255)},
                 'people_counting': {'enabled': False},
                 },
             }
