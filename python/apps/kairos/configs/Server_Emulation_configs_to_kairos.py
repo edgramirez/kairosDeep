662bff4a4619c4c7f33f475569fb4f03fa67fdb8
Edgar Ramirez <edgcramirez@gmail.com>
2021-11-07 20:51:53 -0600

    new testing config

--- python/apps/kairos/configs/Server_Emulation_configs_to_kairos.py
+++ python/apps/kairos/configs/Server_Emulation_configs_to_kairos.py
@@ -1,39 +1,48 @@
 {
     "DTevar-culhuacan-34:56:fe:a3:99:de":
     {
-        "source": "file:///home/aaeon/Downloads/ejercicio2/video3.mp4",
-        "aforo": 
+        "camera_mac_1": 
         {
-            "enabled": true,
-            "reference_line": 
-            {
-                "coordinates": ["(500, 720)", "(1100, 720)"],
-                "reference_line_width": 5,
-                "reference_line_color": ["1", "1", "1", "1"],
-                "outside_area": 1
-            },
-            "area_of_interest": 
-            {
-                "area_of_interest_type": "horizontal",
-                "up": 90,
-                "down": 90,
-                "left": 0,
-                "right": 0
-            }
+	        "aforo": 
+	        {
+	            "enabled": true,
+	            "source": "file:///tmp/shopping_mall.mkv",
+	            "reference_line": 
+	            {
+	                "coordinates": ["(100, 320)", "(500, 320)"],
+	                "reference_line_width": 5,
+	                "reference_line_color": ["1", "1", "1", "1"],
+	                "outside_area": 1
+	            },
+	            "area_of_interest": 
+	            {
+	                "area_of_interest_type": "horizontal",
+	                "up": 90,
+	                "down": 90,
+	                "left": 0,
+	                "right": 0
+	            }
+	        },
+	        "social_distance": 
+	        {
+	            "enabled": false,
+	            "tolerated_distance": 100,
+	            "persistence_time": 1
+	        }
         },
-        "social_distance": 
+        "camera_mac_2":
         {
-            "enabled": false,
-            "tolerated_distance": 100,
-            "persistence_time": 1
+	        "people_counting": 
+	        {
+	            "enabled": false
+	        }
         },
-        "people_counting": 
+        "camera_mac_3":
         {
-            "enabled": false
-        },
-        "mask_detection": 
-        {
-            "enabled": false
+	        "mask_detection": 
+	        {
+	            "enabled": false
+	        }
         }
     }
 }
