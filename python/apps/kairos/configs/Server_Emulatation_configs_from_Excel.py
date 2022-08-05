dff6da096f4bde4ea214c8f23f50cf47365d5961
aaeon <edgcramirez@gmail.com>
2022-08-04 20:29:55 -0500

    V2 - before V3

---
+++ python/apps/kairos/configs/Server_Emulatation_configs_from_Excel.py
@@ -0,0 +1,15 @@
+{
+        'DTevar-culhuacan-34:56:fe:a3:99:de': 
+        {
+            'video-people': 
+            {
+                'reference_line_coordinates': '(500, 720), (1100, 720)', 
+                'MaxAforo': '20.0', 
+                'reference_line_outside_area': '1.0', 
+                'source': 'file:///home/aaeon/shopping_mall.mkv', 
+                'area_of_interest': '90,90,0,0', 
+                'enabled': 'True', 
+                'area_of_interest_type': 'horizontal'
+            }
+        }
+}
